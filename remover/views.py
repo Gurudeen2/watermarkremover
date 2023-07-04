7572343cd134e403c6b0d25ad9cc865246c69b60
Fatai Akeem <akeemtolanifatai@gmail.com>
2023-07-04 23:56:16 +0100

    testing diff code to remove watermark from img

--- remover/views.py
+++ remover/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 import cv2
 from .models import WaterMarkRemove
 from datetime import date
+import numpy as np
 
 # Create your views here.
 
@@ -14,14 +15,29 @@ def removewatermark(request):
         # watermark.save()
 
         today = date.today().strftime("%Y/%m/%d")
-        get_img = WaterMarkRemove.objects.filter(photo=f"photo/{today}/{img}").first().photo
-        print("date",date.today().strftime("%Y/%m/%d"))
-        print("get", get_img)
+        # get_img = WaterMarkRemove.objects.filter(photo=f"photo/{today}/{img}").first().photo
+        # img = cv2.imread(get_img.name, 1)
+
+        # Load image as string from file/database
+        im = open("media/84455cover.jpg", "rb")
+        imgs = im.read()
+        im.close()
+        # fd = open(im)
+        # img_str = fd.read()
+        # fd.close()
+        
         
         # img = "84455cover.jpg"
-        print("path",get_img.name)
-        # img = cv2.imread(get_img.name, 1)
-        # _, thresh =cv2.threshold(img, 150, 255,cv2.THRESH_BINARY)
+        # print("path",get_img.name)
+        imgCv2=cv2.imdecode(np.fromstring(imgs, np.uint8), 1)
+        # imgCv2 = cv2.imread(imgs, 1)
+        # print("get", imgCv2)
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        gray = cv2.adaptiveThreshold(gray, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, 
+                             cv2.THRESH_BINARY, 115, 1)
+        cv2.imshow("Processed image", gray)
+        # _, thresh =cv2.threshold(imgCv2, 150, 255,cv2.THRESH_BINARY)
         # print("thresh", thresh)
         # cv2.imshow("without watermark", thresh)
+        cv2.waitKey(0)
     return render(request, "page/home.html")
