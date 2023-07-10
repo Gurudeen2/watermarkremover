262b7caaecf356d1c7497c54a7f9a65d9f522124
Fatai Akeem <akeemtolanifatai@gmail.com>
2023-07-10 13:28:03 +0100

    middleware for watermark

--- remover/views.py
+++ remover/views.py
@@ -20,6 +20,7 @@ from PIL import Image, ImageDraw, ImageFont
 #         photo.save(self.image.path)
 
 def home(request):
+    print("home la wa")
     return render(request, "page/home.html")
 
 def remove_img_background(request):
@@ -29,32 +30,38 @@ def addwatermark(request):
     if request.method == "POST":
 
         img = request.FILES["image"]
+        word = request.POST["text"]
+
+        print("img", img)
+
         # img = str(img)
         # watermark = WaterMarkRemove.objects.create(photo=img)
         # watermark.save()
 
-        today = date.today().strftime("%Y/%m/%d")
+        # today = date.today().strftime("%Y/%m/%d")
         # get_img = WaterMarkRemove.objects.filter(photo=f"photo/{today}/{img}").first().photo
-        get_img = WaterMarkRemove.objects.filter(photo=f"photo/2023/07/04/{img}").first().photo
 
-        
-        print(get_img.name)
 
-        im = Image.open(get_img.path)
+        im = Image.open(img)
         width, height = im.size
+
+
         text = Image.new("RGBA", im.size, (255, 255, 255, 0))
 
-        font = ImageFont.truetype("arial.ttf", 60)
+        font = ImageFont.truetype("arial.ttf", 50)
         txt = ImageDraw.Draw(text)
         draw = ImageDraw.Draw(im)
-        _, _, w, h, = txt.textbbox((0,0), "Akeem Tolani", font=font)
-        draw.text(((width-w)/2, (height-h)/2), "Akeem Tolani",(255,255,255), font=font, )
-            # Combine the image with text watermark
-        # out = Image.alpha_composite(draw, txt)
-        im.show()
+        _, _, w, h, = txt.textbbox((0,0), word, font=font)
         
+        draw.text(((width-w-10), (height-h-10)), word,(255,255,255), font=font, )
+          
+#         width 728
+# height 410
+# width-w 299
+# height-h 67
+        # # img = str(img)
+        # #updating the image to db
+        # WaterMarkRemove.objects.filter(photo=img).update(photo=im)
       
-        im.save("ak.png")
-
-      
+        return render(request, "page/watermark.html", context={"wimage":im})
     return render(request, "page/watermark.html")
