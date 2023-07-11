4ef1f9c94b800ee8ea357ef95489a071f0f81490
Fatai Akeem <akeemtolanifatai@gmail.com>
2023-07-11 18:32:49 +0100

    middleware for watermark

--- remover/views.py
+++ remover/views.py
@@ -1,9 +1,12 @@
+import sys
 from django.shortcuts import render
 import cv2
 from .models import WaterMarkRemove
 from datetime import date
 import numpy as np
 from PIL import Image, ImageDraw, ImageFont
+from io import BytesIO
+import base64
 
 # Create your views here.
 
@@ -18,6 +21,38 @@ from PIL import Image, ImageDraw, ImageFont
 #         y = height - textheight - margin
 #         draw.text((x,y), myword, (255, 255, 255), font=font)
 #         photo.save(self.image.path)
+def remove_watermark(img_path):
+    image = cv2.imread(img_path)
+
+    im = cv2.imread(sys.path[0]+"/img.jpg", 1)
+    #convert BGR image to RGB(if necessary)
+    img_rgb = cv2.cvtColor(image, cv2.COLOR_BayerGB2RGB)
+
+    #perform inpainting to remove the watermark
+    mask = create_watermark_mask(img_rgb)
+    # masked_image = cv2.bitwise_and(img_rgb, img_rgb, mask=mask)
+    #
+    inpainted_img = inpaint(img_rgb, mask)
+
+    #convert the image back to pil image format
+    modified_image = Image.fromarray(inpainted_img)
+    modified_image.show()
+    return modified_image
+
+def create_watermark_mask(image):
+    lower_threshold = np.array([255, 255, 255], dtype=np.uint8)
+    upper_threshold = np.array([255, 255, 255], dtype=np.uint8)
+    mask = cv2.inRange(image, lower_threshold, upper_threshold) 
+    return mask
+
+def inpaint(image, mask):
+    #perform image inpainting using OpenCV's inpaint function
+
+    inpainted_image = cv2.inpaint(image, mask, 3, cv2.INPAINT_TELEA)
+
+    #fiiled
+    # inpainted_image = fill
+    return inpainted_image
 
 def home(request):
     print("home la wa")
@@ -33,6 +68,10 @@ def addwatermark(request):
         word = request.POST["text"]
 
         print("img", img)
+        m = "media/photo/84455cover.jpg"
+        # u = Image.open(m)
+        # u.show()
+        remove_watermark(m)
 
         # img = str(img)
         # watermark = WaterMarkRemove.objects.create(photo=img)
@@ -53,15 +92,15 @@ def addwatermark(request):
         draw = ImageDraw.Draw(im)
         _, _, w, h, = txt.textbbox((0,0), word, font=font)
         
-        draw.text(((width-w-10), (height-h-10)), word,(255,255,255), font=font, )
-          
-#         width 728
-# height 410
-# width-w 299
-# height-h 67
+        draw.text(((width-w-10), (height-h-10)), word,(255,255,255), font=font)
+
+        bufferpng = BytesIO()
+        im.save(bufferpng, kind="JPEG")
+        img_str = base64.b64encode(bufferpng.getvalue()).decode('utf-8')
+
         # # img = str(img)
         # #updating the image to db
         # WaterMarkRemove.objects.filter(photo=img).update(photo=im)
       
-        return render(request, "page/watermark.html", context={"wimage":im})
+        return render(request, "page/watermark.html", context={"wimage":img_str})
     return render(request, "page/watermark.html")
