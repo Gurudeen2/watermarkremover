2138fc5c15eebbb43db5d5786bc41f88df376298
Fatai Akeem <akeemtolanifatai@gmail.com>
2023-07-15 23:57:11 +0100

    done

--- watermarkconfig/settings.py
+++ watermarkconfig/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = env('SECRETKEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = []
 
 
 # Application definition
