312b380c8152e9737e8019eacc5ad46e9785b739
Fatai Akeem <akeemtolanifatai@gmail.com>
2023-07-16 00:25:02 +0100

    done

--- watermarkconfig/settings.py
+++ watermarkconfig/settings.py
@@ -128,11 +128,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+# STATIC_ROOT = os.path.join(BASE_DIR, "static")
 STATIC_URL = 'static/'
-STATICFILES_DIRS =[
-    os.path.join(BASE_DIR, "watermarkconfig/static")
-]
+# STATICFILES_DIRS =[
+#     os.path.join(BASE_DIR, "watermarkconfig/static")
+# ]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
@@ -142,5 +142,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 #media file config
 
-MEDIA_ROOT = os.path.join(BASE_DIR, "media")
-MEDIA_URL = "/media/"
+# MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+MEDIA_URL = "media/"
