5434bcbb4e0e733c46263671b36e20f9f583a27e
Fatai Akeem <akeemtolanifatai@gmail.com>
2023-07-16 01:03:11 +0100

    done

--- watermarkconfig/urls.py
+++ watermarkconfig/urls.py
@@ -23,4 +23,5 @@ from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", include("remover.urls"))
-] + staticfiles_urlpatterns()
+] 
+urlpatterns += staticfiles_urlpatterns()
